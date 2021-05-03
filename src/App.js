d449e75175442fa10e3ee96ffd243b08d340dc40
olamide oladinni <olajide1987@gmail.com>
2021-05-03 01:43:35 -0500

    search bar rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const APP_ID = '0fafdf64';
+  const APP_KEY = '6ae93647b5822164e497868ab433b2e7';
+
+  // eslint-disable-next-line no-template-curly-in-string
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+
+
+
+
+  return(
+    <div className='App'>
+      <form className='search-form'>
+        <input className='search-bar type=' type='text' />
+        <button className='search-button' type='submit'>Search</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
